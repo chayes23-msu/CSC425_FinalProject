d61a8bb785e5938b73d5dc3b328c2fde6a249564
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2024-11-06 16:22:14 -0600

    added https server

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,44 @@
 import { getQuery, runQuery, eachQuery } from "./database/model/dbUtils.js";
-
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import express from 'express';
+import https from 'https';
+import morgan from 'morgan';
 // ********** Express Server **********
 // This is unfinished starter code for the server that has the endpoints.
 
-// import express from 'express';
+const app = express();
 
-// const server = express();
+// Middleware
+app.use(morgan('dev'));
 
-// const port = 5000;
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// server.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+//Port
+const port = 5000;
+
+// Read SSL certificate and key files
+const options = {
+    key: readFileSync(join("backend", "localhost-key.pem")),
+    cert: readFileSync(join("backend", "localhost.pem")),
+};
+
+// Create https server
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
 // example of testing/usage of a query...all queries will be in the model folder
 // to reference the queries, just use the name of the sql file without the extension
 // look at dbUtils.js for more info on how to use the query functions 
-try {
-    runQuery("createUser", ["idiot", "holmes"]);
-} catch (error) {
-    console.error("Error creating user:", error);
-}
+// try {
+//     runQuery("createUser", ["idiot", "holmes"]);
+// } catch (error) {
+//     console.error("Error creating user:", error);
+// }
