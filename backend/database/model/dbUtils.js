0ac5515ac9a28488dc19d30dbfac4aef34f3b861
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2024-11-05 20:11:04 -0600

    documentation for dbUtils

--- backend/database/model/dbUtils.js
+++ backend/database/model/dbUtils.js
@@ -2,14 +2,35 @@ import db from '../db.js';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
+// ********** Database Utility Functions **********
+// Documentation for the core functions found at https://github.com/TryGhost/node-sqlite3/wiki/API
+
+/**
+ * 
+ * @param {*} queryFileName This is the file name (WITHOUT THE EXTENSION) of the sql you want to run 
+ * @param {*} params This is an array of the parameters you want to pass to the query
+ * @returns Object representing the one row result
+ */
 export function getQuery(queryFileName, params) {
     return getStatementFromSQLFile(queryFileName).get(...params);
 }
 
+/**
+ * 
+ * @param {*} queryFileName This is the file name (WITHOUT THE EXTENSION) of the sql you want to run 
+ * @param {*} params This is an array of the parameters you want to pass to the query
+ * @returns An object summarizing changes made by the query
+ */
 export function runQuery(queryFileName, params) {
     return getStatementFromSQLFile(queryFileName).run(...params);
 }
 
+/**
+ * 
+ * @param {*} queryFileName This is the file name (WITHOUT THE EXTENSION) of the sql you want to run 
+ * @param {*} params This is an array of the parameters you want to pass to the query
+ * @returns Object representing each row returned by the query
+ */
 export function eachQuery(queryFileName, params) {
     return getStatementFromSQLFile(queryFileName).each(...params);
 }
