7118436f41c5c64c6af1dbfb4e1d86ea8575830f
Sherlock1836 <icreynolds325@gmail.com>
2024-11-19 22:02:48 -0600

    moved everything in main to App bc that's what everybody else does apparently

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import './index.css'
 import Login from './Login.jsx'
 import PrivateRoute from './PrivateRoute.jsx';
 import { useAuth } from './AuthProvider.jsx';
+import AuthProvider from './AuthProvider';
+
 
 export default function App() {
     const auth = useAuth();
 
     return (
-        <Routes>
-            <Route path="/" element={
-                auth.token ? <Navigate to="/ex" replace/> : <Navigate to="/login" replace/>
-            } />
-            <Route path="/login" element={
-                auth.token ? <Navigate to="/ex" replace/> : <Login replace/>} />
-            <Route element={<PrivateRoute />}>
-                <Route path="/ex" element={<h1>You've logged in!!!</h1>} />
-            </Route>
-        </Routes>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={
+                        auth?.token ? <Navigate to="/ex" replace /> : <Navigate to="/login" replace />
+                    } />
+                    <Route path="/login" element={
+                        auth?.token ? <Navigate to="/ex" replace /> : <Login replace />} />
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/ex" element={<h1>You&apos;ve logged in!!!</h1>} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AuthProvider from './AuthProvider';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
-			<AuthProvider>
-				<App />
-			</AuthProvider>
-		</BrowserRouter>
+		<App />
 	</StrictMode>,
 );
