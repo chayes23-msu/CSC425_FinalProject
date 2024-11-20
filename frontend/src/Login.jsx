eed1d362c1f0cf7be6b37bea056e5ecdc42c13b2
Sherlock1836 <icreynolds325@gmail.com>
2024-11-20 00:28:18 -0600

    updated login page with mantine component

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,24 +1,33 @@
+import {
+    // Anchor,
+    Button,
+    // Checkbox,
+    Container,
+    // Group,
+    Paper,
+    PasswordInput,
+    // Text,
+    TextInput,
+    Title,
+  } from '@mantine/core';
+import classes from './Login.module.css';
 import { useAuth } from "./AuthProvider";
 import { useState } from "react";
 
 export default function Login() {
     const auth = useAuth();
     const [loginError, setLoginError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-    
-        const formData = new FormData(event.target);
-        const username = formData.get("username");
-        const password = formData.get("password");
-    
+    async function handleSubmit() {
         if (!username || !password) {
             setLoginError("Username and password are required.");
             return;
         }
-    
+
         console.log(`User ${username} attempting log in`);
-    
+
         try {
             await auth.login({ username, password });
             console.log(`User ${username} logged in`);
@@ -30,56 +39,57 @@ export default function Login() {
         }
     }
 
+    // Handle Enter key press to submit form
+    function handleKeyDown(event) {
+        if (event.key === "Enter") {
+            if(username && password)
+                handleSubmit();
+        }
+    }
+
     return (
 
-        <div>
-            <h2>
-                Sign in to your account
-            </h2>
+        <Container size={420} my={40}>
+            <Title ta="center" className={classes.title}>
+                Welcome to Cow App!
+            </Title>
+            {/* <Text c="dimmed" size="sm" ta="center" mt={5}>
+                Do not have an account yet?{' '}
+                <Anchor size="sm" component="button">
+                    Create account
+                </Anchor>
+            </Text> */}
 
-            <form onSubmit={handleSubmit} >
-                <div>
-                    <label htmlFor="username" >
-                        Username
-                    </label>
-                    <div >
-                        <input
-                            id="username"
-                            name="username"
-                            type="text"
-                            required
-                            autoComplete="username"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    <div >
-                        <label htmlFor="password">
-                            Password
-                        </label>
-                    </div>
-                    <div>
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            required
-                            autoComplete="current-password"
-                        />
-                    </div>
-                </div>
-
-                <div>
-                    {loginError && <p>{loginError}</p>}
-                </div>
-
-                <div>
-                    <button type="submit">
-                        Sign in
-                    </button>
-                </div>
-            </form>
-        </div>
+            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                <TextInput 
+                    label="Username" 
+                    placeholder="Your username" 
+                    value={username} 
+                    onChange={(event) => {setUsername(event.currentTarget.value)}} 
+                    onKeyDown={handleKeyDown}
+                    required 
+                    error={loginError !== ""}
+                />
+                <PasswordInput 
+                    label="Password" 
+                    placeholder="Your password" 
+                    required 
+                    mt="md" 
+                    value={password}
+                    onChange={(event) => {setPassword(event.currentTarget.value)}}
+                    onKeyDown={handleKeyDown}
+                    error={loginError}
+                />
+                {/* <Group justify="space-between" mt="lg">
+                    <Checkbox label="Remember me" />
+                    <Anchor component="button" size="sm">
+                        Forgot password?
+                    </Anchor>
+                </Group> */}
+                <Button fullWidth mt="xl" onClick={handleSubmit}>
+                    Sign in
+                </Button>
+            </Paper>
+        </Container>
     )
 }
