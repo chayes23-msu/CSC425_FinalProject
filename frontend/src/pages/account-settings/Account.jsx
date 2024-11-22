0b793c762615a76f49142f567fa90c0207996785
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2024-11-21 20:52:46 -0600

    starting work on Account page

--- frontend/src/pages/account-settings/Account.jsx
+++ frontend/src/pages/account-settings/Account.jsx
@@ -1,3 +1,28 @@
+import { useForm } from '@mantine/form';
+import { Button, Container, TextInput, Title, PasswordInput } from '@mantine/core';
+import { FinalProjectAPI as api } from '../../apis/FinalProjectAPI';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../../authentication/AuthProvider';
+
 export default function Account() {
-    
+    const auth = useAuth();
+    const [userData, setUserData] = useState(auth.user);
+
+    const updateUserData = async () => {
+        try {
+            const resp = await api.getUser(auth.user.userID);
+            setUserData(resp);
+        } catch (err) {
+            console.error(err);
+        }
+    };  
+
+    const form = useForm({
+        mode: 'uncontrolled',
+        initialValues: {
+            username: auth.user.username,
+            newPassword: '',
+            confirmPassword: ''
+        }
+    });
 }
